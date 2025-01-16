2e1a2732155a6afb26101fdea3156d996379cafe
katsuta1104 <evening698564@gmail.com>
2025-01-16 15:10:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-
+const box = document. querySelector("#prime-factorization");
+const result = document. querySelector "#result");
+const checkDivisors = () => {
+let value = box.value;
+let exponent = 0;
+let divisors = [];
+for (let number = 2 ; number <= value; number++) {
+if (value % number === 0) {
+exponent = 0:
+while (value % number === 0) {
+exponent++;
+value /= number;
+}
+divisors.push ({number, exponent});
+}
+box. value = null;
+result.textContent = null;
+divisors. forEach ((divisor, index) =>{
+if (index === divisors. length-1) {
+result.textContent += '${divisor.number}^${divisor.exponent}';
+return
+}
+result.textContent += '${divisor.number}^${divisor.exponent}\n*\n*
+box. addEventListener('keydown' ,e => {
+if (e. key === 'Enter') {
+checkDivisors();
+})
